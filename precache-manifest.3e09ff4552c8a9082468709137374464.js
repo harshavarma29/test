95f9b0b9538c5f106fe712125ce166387f105ede
harshavarma29 <harshavarma29@gmail.com>
2020-08-08 21:52:21 +0530

    Updates

---
+++ precache-manifest.3e09ff4552c8a9082468709137374464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1452f7b0cb0444d9ba44cf9b52c056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fd18b17d63746448730",
+    "url": "/static/css/main.52fe3e30.chunk.css"
+  },
+  {
+    "revision": "bc6ce3e5c7c8db224c18",
+    "url": "/static/js/2.95850076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95850076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd18b17d63746448730",
+    "url": "/static/js/main.41dd3893.chunk.js"
+  },
+  {
+    "revision": "afef85084f0804340619",
+    "url": "/static/js/runtime-main.2b370e64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
